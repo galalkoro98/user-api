e964c5606e0e8975565312d92d2a06de5e06ec94
galalkoro98 <galal.koro@gmail.com>
2023-01-08 00:39:00 +0100

    add reset logic

---
+++ src/reset.js
@@ -0,0 +1,13 @@
+import { resetBtn } from "./template.js";
+// import { resetBtn, container, searchInput } from "./template.js";
+
+// reset button
+resetBtn.addEventListener("click", function () {
+  // searchInput.value = "";
+  // container.innerHTML = "";
+  window.location.reload();
+
+  //   console.log("reset");
+});
+
+// refresh the page
